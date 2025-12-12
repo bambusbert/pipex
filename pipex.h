0e813c3646d555c8a95c052c03ad81e5aa1be7d6
slambert <slambert@student.42vienna.com>
2025-12-12 14:38:37 +0100

    asdf

--- pipex.h
+++ pipex.h
@@ -1,17 +1,18 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slambert <slambert@student.42vienna.com    +#+  +:+       +#+        */
+/*   By: slambert <slambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 12:51:42 by slambert          #+#    #+#             */
-/*   Updated: 2025/12/11 19:20:17 by slambert         ###   ########.fr       */
+/*   Updated: 2025/12/12 13:22:15 by slambert         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "libft/libft.h"
 #include <sys/wait.h>
 #include <unistd.h>
 
-void do_execve_stuff(char *str, char* envp);
+void do_execve_stuff(char *str, char** envp);
+char *extract_path_from_envp(char **envp);
--- pipex_utils.c
+++ pipex_utils.c
@@ -1,14 +1,14 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   pipex_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slambert <slambert@student.42vienna.com    +#+  +:+       +#+        */
+/*   By: slambert <slambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 12:51:33 by slambert          #+#    #+#             */
-/*   Updated: 2025/12/11 19:23:59 by slambert         ###   ########.fr       */
+/*   Updated: 2025/12/12 14:37:09 by slambert         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "pipex.h"
 #include <stdio.h>
@@ -16,20 +16,35 @@
 void do_execve_stuff(char *str, char** envp)
 {
 /*     execve needs 3 variables, 
-    var1:  char* path
-    var2: char* argv[]
+    var1:  char* path: the path where the specific program is stored
+    var2: char* argv[]: contains the program to be launched and its flags
         argv[0]: command (e.g. grep)
         argv[1]: flag 1
         argv[2]: flag 2*
         argv[n]: flag n
         argv[n+1]: NULL
-    var3: envp: ???*/
-    char *path;
+    var3: envp: thats an array of string pointing to the environment paths. this variable gets set automatically from main and is passed through*/
+    char *path_var;
     char **strs;
+
+    path_var = extract_path_from_envp(envp);
     strs = ft_split(str, ' ');
-    path = strs[0]; //TODO add path before
-    strs++;
+    dprintf(STDERR_FILENO, "do_execve_stuff: cmd str='%s'\n", str);
     execve(path, strs, envp);
+}
+
+char *extract_path_from_envp(char **envp)
+{
+    int i;
+
+    i = 0;
+    while (envp[i])
+    {
+        if (!ft_strncmp(envp[i], "PATH=", 5))
+            return envp[i] + 5;
+        i++;
+    }
+    return NULL;
 }
 
 /* char    *substr_until_char(char *s, char c)
